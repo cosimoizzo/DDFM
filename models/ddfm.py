5d3ebf6459bf3bdcdbb02d002336fd5df807c544
cosimo <cosimo.izzo.18.@ucl.ac.uk>
2023-04-13 00:14:05 +0100

    Computing scores on factor estimated via the algorithm in the paper only.

--- models/ddfm.py
+++ models/ddfm.py
@@ -388,8 +388,3 @@ if __name__ == "__main__":
     precision_score = np.trace(f[n_lags + 1:].T @ f_hat @ np.linalg.pinv(f_hat.T @ f_hat) @ f_hat.T @ f[n_lags + 1:]) / \
                       np.trace(f[n_lags + 1:].T @ f[n_lags + 1:])
     print('Precision score filtered: ', precision_score)
-    f_hat = np.mean(ddfm.factors, axis=0)
-    precision_score = np.trace(
-        f[n_lags:].T @ f_hat @ np.linalg.pinv(f_hat.T @ f_hat) @ f_hat.T @ f[n_lags:]) / np.trace(
-        f[n_lags:].T @ f[n_lags:])
-    print('Precision score non filtered: ', precision_score)
