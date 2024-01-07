501c15ead74f53ad991a8b3c1d815db6c286d550
cosimo <izzo.cosimo.15@gmail.com>
2024-01-07 14:15:27 +0000

    Refactored and added new test.

--- tests/tests_tools/test_monthly_quarterly_layer.py
+++ tests/tests_tools/test_monthly_quarterly_layer.py
@@ -10,12 +10,13 @@ class TestMixedFreqMQLayer(unittest.TestCase):
     """
 
     def setUp(self) -> None:
-        n_obs = 10
-        n_vars = 4
+        n_obs = 10000
         start_quarterly = 2
+        n_vars = 4
         g1 = tf.random.Generator.from_seed(1)
-        self.x = g1.uniform((n_obs, n_vars))
+        self.x = g1.normal(shape=(n_obs, n_vars), mean=0, stddev=1)
         x_numpy = self.x.numpy()
+        self.start_quarterly = start_quarterly
         self.mixed_freq_mq_layer = MixedFreqMQLayer(input_dim=n_vars,
                                                     start_quarterly=start_quarterly)
         self.out_expected = x_numpy.copy()
@@ -44,7 +45,24 @@ class TestMixedFreqMQLayer(unittest.TestCase):
 
     def test_mq_layer(self):
         output_layer = self.mixed_freq_mq_layer(self.x)
-        self.assertAlmostEquals(np.sum(np.abs(self.out_expected - output_layer.numpy())), 0, places=5)
+        self.assertAlmostEquals(np.mean(np.abs(self.out_expected - output_layer.numpy())), 0, places=6)
+
+    def test_mq_layer_plus_dense(self):
+        # define linear model with mixed frequency output layer
+        model = tf.keras.Sequential([
+            tf.keras.layers.Dense(self.x.shape[1], bias_initializer='zeros',
+                                  use_bias=False),
+            MixedFreqMQLayer(input_dim=self.out_expected.shape[1],
+                             start_quarterly=self.start_quarterly)
+        ])
+        model.compile(loss=tf.losses.MeanSquaredError(),
+                      optimizer=tf.keras.optimizers.SGD(learning_rate=0.005))
+        model.fit(self.x, self.out_expected, epochs=3000,
+                  batch_size=self.x.shape[0])
+        weights = model.get_weights()
+        # True value of the parameter of the first layer is identity matrix, checking mean absolute error below 0.05.
+        self.assertLessEqual(np.mean(np.abs(weights[0] - np.eye(self.x.shape[1]))), 0.05,
+                             msg=f"value: {weights[0]}")
 
 
 if __name__ == '__main__':
--- tools/monthly_quarterly_layer.py
+++ tools/monthly_quarterly_layer.py
@@ -6,17 +6,18 @@ class MixedFreqMQLayer(keras.layers.Layer):
     """
     A layer implementing monthly quarterly mixed frequency aggregation.
     """
+
     def __init__(self, input_dim: int, start_quarterly: int):
         super().__init__()
         mm_mq = tf.concat([tf.concat([tf.eye(start_quarterly),
-                                      tf.zeros((5 * input_dim-start_quarterly, start_quarterly))], axis=0),
+                                      tf.zeros((5 * input_dim - start_quarterly, start_quarterly))], axis=0),
                            # monthly to quarterly aggregation
-                           tf.concat([0 * tf.eye(start_quarterly, num_columns=input_dim-start_quarterly),
-                                      1 * tf.eye(input_dim, num_columns=input_dim-start_quarterly),
-                                      2 * tf.eye(input_dim, num_columns=input_dim-start_quarterly),
-                                      3 * tf.eye(input_dim, num_columns=input_dim-start_quarterly),
-                                      2 * tf.eye(input_dim, num_columns=input_dim-start_quarterly),
-                                      1 * tf.eye(input_dim-start_quarterly, num_columns=input_dim-start_quarterly),
+                           tf.concat([0 * tf.eye(start_quarterly, num_columns=input_dim - start_quarterly),
+                                      1 * tf.eye(input_dim, num_columns=input_dim - start_quarterly),
+                                      2 * tf.eye(input_dim, num_columns=input_dim - start_quarterly),
+                                      3 * tf.eye(input_dim, num_columns=input_dim - start_quarterly),
+                                      2 * tf.eye(input_dim, num_columns=input_dim - start_quarterly),
+                                      1 * tf.eye(input_dim - start_quarterly, num_columns=input_dim - start_quarterly),
                                       ],
                                      axis=0)],
                           axis=1)
@@ -28,19 +29,37 @@ class MixedFreqMQLayer(keras.layers.Layer):
         self.b = self.add_weight(shape=(input_dim,), initializer="zeros", trainable=False)
 
     def call(self, inputs):
-        inputs_and_lags = tf.concat([inputs,
-                                     tf.multiply(tf.roll(inputs, shift=1, axis=0, name=None), tf.concat(
-                                         [tf.zeros((1, inputs.shape[1])),
-                                          tf.ones((inputs.shape[0] - 1, inputs.shape[1]))], axis=0)),
-                                     tf.multiply(tf.roll(inputs, shift=2, axis=0, name=None), tf.concat(
-                                         [tf.zeros((2, inputs.shape[1])),
-                                          tf.ones((inputs.shape[0] - 2, inputs.shape[1]))], axis=0)),
-                                     tf.multiply(tf.roll(inputs, shift=3, axis=0, name=None), tf.concat(
-                                         [tf.zeros((3, inputs.shape[1])),
-                                          tf.ones((inputs.shape[0] - 3, inputs.shape[1]))], axis=0)),
-                                     tf.multiply(tf.roll(inputs, shift=4, axis=0, name=None), tf.concat(
-                                         [tf.zeros((4, inputs.shape[1])),
-                                          tf.ones((inputs.shape[0] - 4, inputs.shape[1]))], axis=0)),
-                                     ],
-                                    axis=1)
-        return tf.matmul(inputs_and_lags, self.w) + self.b
+        return custom_op(inputs, self.w)
+
+
+@tf.custom_gradient
+def custom_op(x, weights):
+    x_ext = get_input(x)
+    result = tf.matmul(x_ext, weights)
+
+    def custom_grad(upstream):
+        inputs_gradient = tf.matmul(get_input(upstream), weights)
+        weights_gradient = tf.matmul(tf.transpose(get_input(x)), get_input(upstream))
+        return inputs_gradient, weights_gradient
+
+    return result, custom_grad
+
+
+@tf.function
+def get_input(inputs):
+    inputs_and_lags = tf.concat([inputs,
+                                 tf.multiply(tf.roll(inputs, shift=1, axis=0, name=None), tf.concat(
+                                     [tf.zeros((1, inputs.shape[1])),
+                                      tf.ones((inputs.shape[0] - 1, inputs.shape[1]))], axis=0)),
+                                 tf.multiply(tf.roll(inputs, shift=2, axis=0, name=None), tf.concat(
+                                     [tf.zeros((2, inputs.shape[1])),
+                                      tf.ones((inputs.shape[0] - 2, inputs.shape[1]))], axis=0)),
+                                 tf.multiply(tf.roll(inputs, shift=3, axis=0, name=None), tf.concat(
+                                     [tf.zeros((3, inputs.shape[1])),
+                                      tf.ones((inputs.shape[0] - 3, inputs.shape[1]))], axis=0)),
+                                 tf.multiply(tf.roll(inputs, shift=4, axis=0, name=None), tf.concat(
+                                     [tf.zeros((4, inputs.shape[1])),
+                                      tf.ones((inputs.shape[0] - 4, inputs.shape[1]))], axis=0)),
+                                 ],
+                                axis=1)
+    return inputs_and_lags
